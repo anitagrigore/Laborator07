31d619ccfafa8beea5128866862746b07007cdc4
Anita Grigore <anita.grigore1@gmail.com>
2022-05-02 11:53:20 +0300

    Add solution for ex 4

--- labtasks/CalculatorWebService/app/src/main/java/ro/pub/cs/systems/eim/lab07/calculatorwebservice/network/CalculatorWebServiceAsyncTask.java
+++ labtasks/CalculatorWebService/app/src/main/java/ro/pub/cs/systems/eim/lab07/calculatorwebservice/network/CalculatorWebServiceAsyncTask.java
@@ -1,7 +1,26 @@
 package ro.pub.cs.systems.eim.lab07.calculatorwebservice.network;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.ClientProtocolException;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.protocol.HTTP;
+import ro.pub.cs.systems.eim.lab07.calculatorwebservice.general.Constants;
 
 public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, String> {
 
@@ -17,14 +36,84 @@ public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, Strin
         String operator2 = params[1];
         String operation = params[2];
         int method = Integer.parseInt(params[3]);
+        String errorMsg = null;
 
         // TODO exercise 4
         // signal missing values through error messages
+        if (operator1 == null || operator1.isEmpty()) {
+            errorMsg = Constants.ERROR_MESSAGE_EMPTY;
+        }
+        if (operator2 == null || operator2.isEmpty()) {
+            errorMsg = Constants.ERROR_MESSAGE_EMPTY;
+        }
+        if (operation == null || operation.isEmpty()) {
+            errorMsg = Constants.ERROR_MESSAGE_EMPTY;
+        }
+        if (errorMsg != null) {
+            return errorMsg;
+        }
 
         // create an instance of a HttpClient object
+        HttpClient httpClient = new DefaultHttpClient();
 
         // get method used for sending request from methodsSpinner
 
+        switch (method) {
+            case Constants.GET_OPERATION:
+                HttpGet httpGet = new HttpGet(Constants.GET_WEB_SERVICE_ADDRESS +
+                        "?" + Constants.OPERATION_ATTRIBUTE + "=" + operation +
+                        "&" + Constants.OPERATOR1_ATTRIBUTE + "=" + operator1 +
+                        "&" + Constants.OPERATOR2_ATTRIBUTE + "=" + operator2);
+                ResponseHandler<String> responseHandlerGet = new BasicResponseHandler();
+
+                try {
+                    return httpClient.execute(httpGet, responseHandlerGet);
+                } catch (ClientProtocolException clientProtocolException) {
+                    Log.e(Constants.TAG, clientProtocolException.getMessage());
+                    if (Constants.DEBUG) {
+                        clientProtocolException.printStackTrace();
+                    }
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+                break;
+
+            case Constants.POST_OPERATION:
+                HttpPost httpPost = new HttpPost(Constants.POST_WEB_SERVICE_ADDRESS);
+                List<NameValuePair> attributes = new ArrayList<>();
+                attributes.add(new BasicNameValuePair(Constants.OPERATION_ATTRIBUTE, operation));
+                attributes.add(new BasicNameValuePair(Constants.OPERATOR1_ATTRIBUTE, operator1));
+                attributes.add(new BasicNameValuePair(Constants.OPERATOR2_ATTRIBUTE,operator2));
+
+                try {
+                    UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(attributes, HTTP.UTF_8);
+                    httpPost.setEntity(urlEncodedFormEntity);
+                } catch (UnsupportedEncodingException unsupportedEncodingException) {
+                    Log.e(Constants.TAG, unsupportedEncodingException.getMessage());
+                    if (Constants.DEBUG) {
+                        unsupportedEncodingException.printStackTrace();
+                    }
+                }
+
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                try {
+                    return httpClient.execute(httpPost, responseHandler);
+                } catch (ClientProtocolException clientProtocolException) {
+                    Log.e(Constants.TAG, clientProtocolException.getMessage());
+                    if (Constants.DEBUG) {
+                        clientProtocolException.printStackTrace();
+                    }
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+                break;
+        }
         // 1. GET
         // a) build the URL into a HttpGet object (append the operators / operations to the Internet address)
         // b) create an instance of a ResultHandler object
@@ -43,6 +132,7 @@ public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, Strin
     @Override
     protected void onPostExecute(String result) {
         // display the result in resultTextView
+        resultTextView.setText(result);
     }
 
 }
